df4ba782f1e55528aab94d56127237532016fa11
Lyall Sun <lyall.sunlin@gmail.com>
2017-07-11 15:29:58 +0800

    Refactored the error handler machanism

--- example1/server.js
+++ example1/server.js
@@ -80,24 +80,67 @@ BND.configureRepository({
 		});
 
 
-		const htmlPath = path.resolve(__dirname, './client/index.html');
-		const serviceSPA = await BND.registerService({
-		    name: 'SPA',
-		    settings: {
-		    	path: '/*',
-		    	method: 'get',
-		    	response: {
-		        	type: 'html',
-		        	path: htmlPath,
-		    	},
-			},
-			routes: routes
-		});
+		
 
+
+		// const serviceGreetings = BND.registerService({
+		// 	name: 'greetings',
+		// 	handler: (name, age, secret, cookies, ctx) => {
+		// 		console.log('secret:', secret);
+		// 		cookies.set('secret', 'This is a cookie set by cookie');
+		// 		// ctx.cookies.set('secret', 'this is a cookie set by ctx');
+		// 		if (!name || !age) {
+		// 			return Promise.reject('参数错误，必须输入姓名和年龄，缺一不可')
+		// 		}
+		// 		let numAge = age;
+		// 		if (typeof numAge !== 'number') {
+		// 			try {
+		// 				numAge = Number(age);
+		// 			} catch (err) {
+		// 				return Promise.reject('年龄必须为阿拉伯数字');
+		// 			}
+		// 		}
+		// 		if (numAge <= 0) {
+		// 			return Promise.reject('年龄必须大于 0');
+		// 		} else if (numAge < 18) {
+		// 			return Promise.reject(`对不起 ${name}，您尚未成年`);
+		// 		} else if (numAge < 30) {
+		// 			return Promise.resolve(`欢迎 ${name}！您真年轻！`);
+		// 		} else {
+		// 			return Promise.reject(`对不起 ${name}，您有点老了`);
+		// 		}
+		// 	},
+		// 	settings: {
+		// 		path: '/greetings',
+		// 		method: 'put',
+		// 		inputParameters: [{
+		// 			position: 'body',
+		// 			type: 'string',
+		// 			name: 'username',
+		// 		}, {
+		// 			position: 'body',
+		// 			type: 'number',
+		// 			name: 'age',
+		// 		}, {
+		// 			position: 'cookies',
+		// 			type: 'string',
+		// 			name: 'secret',
+		// 		}, {
+		// 			position: 'cookies',
+		// 			type: 'object',
+		// 		}, {
+		// 			position: 'ctx',
+		// 			type: 'object',
+		// 		}]
+		// 	},
+		// 	routes
+		// });
 
 		const serviceGreetings = BND.registerService({
 			name: 'greetings',
-			handler: (name, age) => {
+			handler: (name, age, secret, cookies, ctx) => {
+				console.log('secret:', secret);
+				cookies.set('secret', 'This is a cookie set by cookie');
 				if (!name || !age) {
 					return Promise.reject('参数错误，必须输入姓名和年龄，缺一不可')
 				}
@@ -121,18 +164,43 @@ BND.configureRepository({
 			},
 			settings: {
 				path: '/greetings',
-				method: 'put',
+				method: 'get',
 				inputParameters: [{
-					position: 'body',
+					position: 'query',
 					type: 'string',
 					name: 'username',
 				}, {
-					position: 'body',
+					position: 'query',
 					type: 'number',
 					name: 'age',
-				}]
+				}, {
+					position: 'cookies',
+					type: 'string',
+					name: 'secret',
+				}, {
+					position: 'cookies',
+					type: 'object',
+				}, {
+					position: 'ctx',
+					type: 'object',
+				} ]
 			},
 			routes
+		});
+
+
+		const htmlPath = path.resolve(__dirname, './client/index.html');
+		const serviceSPA = await BND.registerService({
+		    name: 'SPA',
+		    settings: {
+		    	path: '/*',
+		    	method: 'get',
+		    	response: {
+		        	type: 'html',
+		        	path: htmlPath,
+		    	},
+			},
+			routes: routes
 		});
 	} catch (error) {
 		console.log('service error:', error);
